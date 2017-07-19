a27353b1d1e3145411d86766e345984f834cb76e
Sotos Matzanas <sotiris.matzanas@gmail.com>
2017-07-19 13:29:30 +0200

    Tests finally run

--- src/test/scala/S99ListsTest.scala
+++ src/test/scala/S99ListsTest.scala
@@ -1,5 +1,8 @@
+import org.scalatest.junit.JUnitRunner
 import org.scalatest.{FlatSpec, Matchers}
+import org.junit.runner.RunWith
 
+@RunWith(classOf[JUnitRunner])
 class S99ListsTest extends FlatSpec with Matchers {
 
   "P01" should "find the last element of a list" in {
